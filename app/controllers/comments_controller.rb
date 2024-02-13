1fce13d446afe7b8c1c2b2b0099e39b3768e06e7
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-13 10:27:38 +0300

    add all resources for articles controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,39 @@
 class CommentsController < ApplicationController
 
+    def index 
+        comment = Comment.all
+        render json: comment, status: :ok
+    end
+
     def create
         comment = Comment.create(comment_params)
         render json: comment, status: :created
     end
 
+    def show
+        comment = find_comment
+        render json: comment
+    end
+
+    def update
+        comment = find_article
+        comment.update(comment_params)
+        render json: comment, status: :ok
+    end
+
+    def destroy
+        comment = find_comment
+        comment.destroy
+        head: no_content
+    end
+
     private
 
     def comment_params
-        params.permit()
+        params.permit(:commenter, :body)
+    end
+
+    def find_comment
+        Comment.find(params[:id])
     end
 end
