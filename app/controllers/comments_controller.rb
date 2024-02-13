41c49c89f59e36b4e0be983ce712c6e678ad88ce
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-13 10:15:22 +0300

    add comments controller

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,13 @@
+class CommentsController < ApplicationController
+
+    def create
+        comment = Comment.create(comment_params)
+        render json: comment, status: :created
+    end
+
+    private
+
+    def comment_params
+        params.permit()
+    end
+end
