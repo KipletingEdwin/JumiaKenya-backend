fcc7bd816b2cb48151c2434628a480dc78a3139e
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-02-13 08:38:57 +0300

    add update and destroy method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,6 +10,23 @@ class ArticlesController < ApplicationController
   render json: article, status: :created
   end
 
+  def show
+    article = find_article
+    render json: article
+  end
+
+  def update
+    article = find_article
+    article.update(article_params)
+    render json: article, status: :ok
+  end
+
+  def destroy
+    article = find_article
+    article.destroy
+    head :no_content
+  end
+
 
   private
 
@@ -20,5 +37,8 @@ class ArticlesController < ApplicationController
   def find_article
     Article.find(params[:id])
   end
+
+  def render_unprocessable_entity_response(exception)
+    render json: {errors: exception.record.errors.full_messags }, status: :unprocessable_entity
 
 end
